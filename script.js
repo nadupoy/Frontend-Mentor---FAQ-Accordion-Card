42b8fbdcb3866ea252d8d532761eab3a764ec290
Nadupoy <gnsampao@gmail.com>
2023-10-12 20:23:03 +0300

    First commit of 'code-restructure' branch.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 "use strict";
-/*
-1. Switch the display property of '.answer' between 'none' and 'block'.
-Can be achieved by targeting the sibling element of '.question_and_icon' as that is what will be receiving the 'onclick' event.
-2. Switch the font-weight & font-color of '.question'.
-3. Switch the position of '.box-illustration-desktop'.
-4. Switch the rotation of the arrow icon.
-*/
 
-const frequentlyAskedQuestions = document.querySelectorAll('.question_and_icon');
+const details = document.querySelectorAll('details');
+const questions = document.querySelectorAll('.questions');
 
-function displayAnswer() {
-    this.nextElementSibling.classList.toggle('display_answer');
-    this.firstElementChild.classList.toggle('question_focus');
-    this.lastElementChild.classList.toggle('rotate_icon');
-}
+for (let detail of details) {
+    detail.addEventListener('toggle', () => {
+        let question = detail.firstElementChild;
+        question.classList.toggle('questions-toggled');
 
-for (let frequentlyAskedQuestion of frequentlyAskedQuestions) {
-    frequentlyAskedQuestion.addEventListener('click', displayAnswer);
+        let arrowIcon = question.firstElementChild;
+        arrowIcon.classList.toggle('arrow-icon-toggled');
+    });
+
+    const boxIllustration = document.querySelector('.box-illustration');
+
+    detail.addEventListener('mouseenter', () => {
+        boxIllustration.style.marginRight = '-60px';
+    });
+
+    detail.addEventListener('mouseleave', () => {
+        boxIllustration.style.marginRight = '-80px';
+    });
 }
